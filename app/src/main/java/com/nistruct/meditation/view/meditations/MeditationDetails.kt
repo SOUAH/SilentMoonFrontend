1b7fe1b7ea3d35be419c118030a1fdc930335e6a
ilij4 <ilijammilosavljevic@gmail.com>
2023-06-05 12:07:31 +0200

    music url

--- app/src/main/java/com/nistruct/meditation/view/meditations/MeditationDetails.kt
+++ app/src/main/java/com/nistruct/meditation/view/meditations/MeditationDetails.kt
@@ -5,14 +5,33 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -195,7 +214,7 @@ fun Content(
                 Column {
                     SongList(
 
-                     meditation.playlist, navController
+                        meditation.playlist, navController
                     )
                 }
             }
@@ -220,7 +239,7 @@ fun SongList(
                 item = item,
             ) {
                 val encodedSongUrl = URLEncoder.encode(item.songUrl, "UTF-8")
-                navController.navigate("Music/${item.songName}/${item.songUrl}")
+                navController.navigate("Music/${item.songName}/${encodedSongUrl}")
 
             }
         }
